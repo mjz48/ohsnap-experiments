74a0455c03cb9a3395de2ecc6f6eaf23ecc9fee2
Michael Zhu <mjz48@cornell.edu>
2023-04-03 18:56:39 -0700

    Follow Hassam's tutorial Part 2. Implement broker.
    Create broker that waits on default port for MQTT clients to connect.
    The server does nothing else, just displays a message when a client
    connects.

--- mqttrs-1/README.md
+++ mqttrs-1/README.md
@@ -10,7 +10,17 @@ This reddit thread about [which mqtt rust library to use](https://www.reddit.com
 
 Useful qualities of the mqttrs library also include that it has fewer dependencies (mqttrs, bytes, and all the dependencies those pull in). It is not a wrapper around a pre-compiled library (for example, paho-mqtt is an "FFI wrapper"). This is important because we are prioritizing keeping dependencies (and overall code-size) small. Pulling in dependencies that are closed-source is also a no-go. Endorsement for quality of documentation and ease of use (API simplicity) are good.
 
+### Tokio
+
+This is a rust library for asynchronous code. The mqttrs crate is designed to work with tokio, and it will probably be necessary to pull in this library eventually if we want to allow for asynchronous communication between MQTT clients.
+
+### MQTT CLI by HiveMQ
+
+I was turned on to this tool by Hassam Uddin's MQTT tutorial, Part 2. This is a CLI tool to simulate being an MQTT cilent. Very useful for testing.
+
 ## Resources and Links
 
 1. [Writing an asynchronous MQTT Broker in Rust - Part 1](https://hassamuddin.com/blog/rust-mqtt/overview/)
 2. [MQTT Version 3.1.1 Spec](http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html)
+3. [Tokio (Getting Started)](https://tokio.rs/tokio/tutorial/hello-tokio)
+4. [HiveMQ's MQTT CLI](https://hivemq.github.io/mqtt-cli/docs/installation/)
--- mqttrs-1/src/main.rs
+++ mqttrs-1/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("Starting MQTT broker...");
+
+    // listen on tcp port using tokio
+    let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 1883);
+    let mut listener = TcpListener::bind(address).await?;
+
+    loop {
+        // TODO: need to handle connection failure
+        let (stream, addr) = listener.accept().await.unwrap();
+        println!("New connection: {}", address);
+
+        tokio::spawn(async {
+            handle_client().await;
+        });
+    }
 }
+
+async fn handle_client() {
+    println!("New client thread spawned")
+}
+
+// TODO: this is from mqttrs example (Delete or incorporate at some point)
+//use bytes::BytesMut;
+//use mqttrs::*;
+//
+//fn main() {
+//    // Allocate write buffer
+//    let mut buf = BytesMut::with_capacity(1024);
+//    
+//    // Encode an MQTT Connect packet
+//    let pkt = Packet::Connect(
+//        Connect {
+//            protocol: Protocol::MQTT311,
+//            keep_alive: 30,
+//            client_id: "doc_client".into(),
+//            clean_session: true,
+//            last_will: None,
+//            username: None,
+//            password: None
+//        }
+//    );
+//    
+//    assert!(encode_slice(&pkt, &mut buf).is_ok());
+//    assert_eq!(&buf[14..], "doc_client".as_bytes());
+//    let mut encoded = buf.clone();
+//    
+//    // Decode one packet. The buffer will advance to the next packet.
+//    assert_eq!(Ok(Some(pkt)), decode_slice(&mut buf));
+//}    
