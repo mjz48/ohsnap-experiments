50050413b2f9fa75f6c68c5c6107272876754351
Michael Zhu <mjz48@cornell.edu>
2023-05-20 16:14:21 -0700

    Run rustfmt on main.rs.

--- mqtt-cli/src/main.rs
+++ mqtt-cli/src/main.rs
@@ -1,7 +1,7 @@
 use mqtt_cli::cli::command::operand::error::MissingOperandError;
+use mqtt_cli::cli::shell::{self, Shell};
 use mqtt_cli::cli::spec;
 use mqtt_cli::cli::spec::flag;
-use mqtt_cli::cli::shell::{self, Shell};
 use mqtt_cli::commands;
 use mqtt_cli::mqtt;
 
@@ -9,27 +9,29 @@ fn main() {
     let add = spec::Command::build("add")
         .set_help("Add two numbers together")
         .add_flag("verbose", 'v', spec::Arg::default(), "Print more info")
-        .add_flag( "modulo", 'm', spec::Arg::Required, "Perform modulo on the resulting addition")
-        .set_callback(| command, _shell, _state, _context | {
+        .add_flag(
+            "modulo",
+            'm',
+            spec::Arg::Required,
+            "Perform modulo on the resulting addition",
+        )
+        .set_callback(|command, _shell, _state, _context| {
             let operands = command.operands();
             let expected_num_operands = 2;
 
             if operands.len() != expected_num_operands {
-                return Err(Box::new(
-                    MissingOperandError(
-                        operands[..].into(),
-                        expected_num_operands,
-                    )
-                ));
+                return Err(Box::new(MissingOperandError(
+                    operands[..].into(),
+                    expected_num_operands,
+                )));
             }
 
             let res = operands[0].get_as::<i32>()? + operands[1].get_as::<i32>()?;
-            let modulo =
-                if let Some(modulo_flag) = command.get_flag(flag::Query::Short('m')) {
-                    modulo_flag.arg().get_as::<i32>()?
-                } else {
-                    None
-                };
+            let modulo = if let Some(modulo_flag) = command.get_flag(flag::Query::Short('m')) {
+                modulo_flag.arg().get_as::<i32>()?
+            } else {
+                None
+            };
             let res = if let Some(m) = modulo { res % m } else { res };
             println!("{}", res);
 
@@ -48,7 +50,7 @@ fn main() {
     command_set.insert(connect.name().to_owned(), connect);
     command_set.insert(ping.name().to_owned(), ping);
 
-    let context = mqtt::MqttContext{
+    let context = mqtt::MqttContext {
         prompt_string: "mqtt".into(),
         client_id: "mqtt-cli".into(),
         broker: mqtt::BrokerAddr::new(),
@@ -64,11 +66,11 @@ fn main() {
     let mut shell_state = shell::State::new();
     shell_state.insert(
         shell::STATE_ON_RUN_COMMAND.into(),
-        shell::StateValue::String("help".into())
+        shell::StateValue::String("help".into()),
     );
     shell_state.insert(
         shell::STATE_PROMPT_STRING.into(),
-        shell::StateValue::String(context.prompt_string.clone())
+        shell::StateValue::String(context.prompt_string.clone()),
     );
 
     shell.run(shell_state, context);
