a517d5bbffb8414d92c862276808cbae88bd7cf1
Michael Zhu <mjz48@cornell.edu>
2023-09-16 01:32:56 -0700

    Implement wrapper type for mqttrs::Packet.
    This type converts all mqttrs::Packet fields that are references into
    owned types. The file also provides conversion functions to and from the
    mqttrs::Packet type.
    
    This is supposed to make it easier to code, having an mqtt::Packet type
    that doesn't require lifetimes and associated buffers everywhere at the
    cost of extra copying and memory fragmentation. The idea is that
    eventually we can get rid of the mqttrs library and implement different
    internals for the mqtt::Packet type and get some of the overhead from
    copying back.

--- mqttrs-1/src/mqtt.rs
+++ mqttrs-1/src/mqtt.rs
@@ -1,17 +1,24 @@
+pub use packet::{
+    Connack, Connect, LastWill, Packet, Pid, Protocol, Publish, QoS, QosPid, Suback, Subscribe,
+    Unsubscribe,
+};
+
 use crate::error::{Error, Result};
 use bytes::Bytes;
 use futures::SinkExt;
-use mqttrs::{encode_slice, Connack, Connect, Packet, Pid, Subscribe, Unsubscribe};
+use mqttrs::{encode_slice, Packet as MqttrsPacket};
 use tokio::net::TcpStream;
 use tokio_util::codec::{BytesCodec, Framed};
 
-/// Given an mqttrs::Packet reference, allocate a byte buffer with the packet
+pub mod packet;
+
+/// Given an mqttrs::MqttrsPacket reference, allocate a byte buffer with the packet
 /// contents encoded into it. This function automatically calculates the size
 /// of the provided packet and returns a vector of the correct size.
 ///
 /// # Arguments
 ///
-/// * `pkt` - mqttrs::Packet reference to encode
+/// * `pkt` - mqttrs::MqttrsPacket reference to encode
 ///
 /// # Errors:
 ///
@@ -19,40 +26,40 @@ use tokio_util::codec::{BytesCodec, Framed};
 ///
 /// * EncodeFailed
 ///
-pub fn encode(pkt: &Packet) -> Result<Vec<u8>> {
-    let sz = std::mem::size_of::<Packet>()
+pub fn encode(pkt: &MqttrsPacket) -> Result<Vec<u8>> {
+    let sz = std::mem::size_of::<MqttrsPacket>()
         + match pkt {
-            Packet::Connack(_) => std::mem::size_of::<Connack>(),
-            Packet::Connect(_) => std::mem::size_of::<Connect>(),
-            Packet::Publish(publish) => {
+            MqttrsPacket::Connack(_) => std::mem::size_of::<Connack>(),
+            MqttrsPacket::Connect(_) => std::mem::size_of::<Connect>(),
+            MqttrsPacket::Publish(publish) => {
                 std::mem::size_of::<mqttrs::Publish>()
                     + std::mem::size_of::<u8>() * publish.payload.len()
             }
-            Packet::Puback(_)
-            | Packet::Pubrec(_)
-            | Packet::Pubrel(_)
-            | Packet::Pubcomp(_)
-            | Packet::Unsuback(_) => std::mem::size_of::<Pid>(),
-            Packet::Subscribe(subscribe) => {
+            MqttrsPacket::Puback(_)
+            | MqttrsPacket::Pubrec(_)
+            | MqttrsPacket::Pubrel(_)
+            | MqttrsPacket::Pubcomp(_)
+            | MqttrsPacket::Unsuback(_) => std::mem::size_of::<Pid>(),
+            MqttrsPacket::Subscribe(subscribe) => {
                 let mut len = std::mem::size_of::<Subscribe>();
                 for t in &subscribe.topics[..] {
                     len += t.topic_path.len();
                 }
                 len
             }
-            Packet::Suback(suback) => {
+            MqttrsPacket::Suback(suback) => {
                 std::mem::size_of::<mqttrs::Suback>()
                     + std::mem::size_of::<mqttrs::SubscribeReturnCodes>()
                         * suback.return_codes.len()
             }
-            Packet::Unsubscribe(unsubscribe) => {
+            MqttrsPacket::Unsubscribe(unsubscribe) => {
                 let mut len = std::mem::size_of::<Unsubscribe>();
                 for t in &unsubscribe.topics[..] {
                     len += t.len();
                 }
                 len
             }
-            Packet::Pingreq | Packet::Pingresp | Packet::Disconnect => 0,
+            MqttrsPacket::Pingreq | MqttrsPacket::Pingresp | MqttrsPacket::Disconnect => 0,
         };
     let mut buf = vec![0u8; sz];
 
@@ -66,13 +73,13 @@ pub fn encode(pkt: &Packet) -> Result<Vec<u8>> {
     Ok(buf)
 }
 
-/// Given an mqttrs::Packet reference, send it across a framed tcp connection.
+/// Given an mqttrs::MqttrsPacket reference, send it across a framed tcp connection.
 /// This function creates and deletes a buffer to encode the packet. (This should
 /// be known to the user to decide if the extra memory allocation is acceptable.)
 ///
 /// # Arguments:
 ///
-/// * `pkt` - an mqttrs::Packet reference to send
+/// * `pkt` - an mqttrs::MqttrsPacket reference to send
 /// * `framed` - a tokio Framed tcp channel to send on
 ///
 /// # Errors:
@@ -80,8 +87,11 @@ pub fn encode(pkt: &Packet) -> Result<Vec<u8>> {
 /// This function may throw the following errors:
 ///
 /// * EncodeFailed
-/// * PacketSendFailed
-pub async fn send(pkt: &Packet<'_>, framed: &mut Framed<TcpStream, BytesCodec>) -> Result<()> {
+/// * MqttrsPacketSendFailed
+pub async fn send(
+    pkt: &MqttrsPacket<'_>,
+    framed: &mut Framed<TcpStream, BytesCodec>,
+) -> Result<()> {
     let buf = encode(pkt)?;
 
     Ok(framed.send(Bytes::from(buf)).await.or_else(|e| {
---
+++ mqttrs-1/src/mqtt/packet.rs
@@ -0,0 +1,168 @@
+pub use mqttrs::{Connack, Pid, Protocol, QoS, QosPid, Suback, Subscribe, Unsubscribe};
+
+/// struct representing Last Will message. Based on mqttrs crate.
+#[derive(Debug, Clone, PartialEq)]
+pub struct LastWill {
+    pub topic: String,
+    pub message: Vec<u8>,
+    pub qos: QoS,
+    pub retain: bool,
+}
+
+/// enum representing MQTT control packets. Based on mqttrs crate.
+#[derive(Debug, Clone, PartialEq)]
+pub enum Packet {
+    Connack(Connack),
+    Connect(Connect),
+    Publish(Publish),
+    Puback(Pid),
+    Pubrec(Pid),
+    Pubrel(Pid),
+    Pubcomp(Pid),
+    Subscribe(Subscribe),
+    Suback(Suback),
+    Unsubscribe(Unsubscribe),
+    Unsuback(Pid),
+    Pingreq,
+    Pingresp,
+    Disconnect,
+}
+
+/// struct representing data from Connect MQTT control packet.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Connect {
+    pub protocol: Protocol,
+    pub keep_alive: u16,
+    pub client_id: String,
+    pub clean_session: bool,
+    pub last_will: Option<LastWill>,
+    pub username: Option<String>,
+    pub password: Option<Vec<u8>>,
+}
+
+/// struct representing data from Publish MQTT control packet.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Publish {
+    pub dup: bool,
+    pub qospid: QosPid,
+    pub retain: bool,
+    pub topic_name: String,
+    pub payload: Vec<u8>,
+}
+
+impl Packet {
+    /// convert between mqtt::Packet and mqttrs::Packet. We can't do this through
+    /// the Into<T> trait because that interface requires ownership of input and
+    /// there is no way to link lifetimes together.
+    pub fn into<'a>(pkt: &'a Packet) -> mqttrs::Packet<'a> {
+        match pkt {
+            Packet::Connack(connack) => mqttrs::Packet::Connack(*connack),
+            Packet::Connect(connect) => mqttrs::Packet::Connect(mqttrs::Connect {
+                protocol: connect.protocol,
+                keep_alive: connect.keep_alive,
+                client_id: &connect.client_id,
+                clean_session: connect.clean_session,
+                last_will: match connect.last_will {
+                    Some(ref lw) => Some(mqttrs::LastWill {
+                        topic: &lw.topic,
+                        message: &lw.message,
+                        qos: lw.qos,
+                        retain: lw.retain,
+                    }),
+                    None => None,
+                },
+                username: match connect.username {
+                    Some(ref username) => Some(username),
+                    None => None,
+                },
+                password: match connect.password {
+                    Some(ref password) => Some(&password),
+                    None => None,
+                },
+            }),
+            Packet::Publish(publish) => mqttrs::Packet::Publish(mqttrs::Publish {
+                dup: publish.dup,
+                qospid: publish.qospid,
+                retain: publish.retain,
+                topic_name: &publish.topic_name,
+                payload: &publish.payload,
+            }),
+            Packet::Puback(pid) => mqttrs::Packet::Puback(*pid),
+            Packet::Pubrec(pid) => mqttrs::Packet::Pubrec(*pid),
+            Packet::Pubrel(pid) => mqttrs::Packet::Pubrel(*pid),
+            Packet::Pubcomp(pid) => mqttrs::Packet::Pubcomp(*pid),
+            Packet::Subscribe(subscribe) => mqttrs::Packet::Subscribe(mqttrs::Subscribe {
+                pid: subscribe.pid,
+                topics: subscribe.topics.clone(),
+            }),
+            Packet::Suback(suback) => mqttrs::Packet::Suback(mqttrs::Suback {
+                pid: suback.pid,
+                return_codes: suback.return_codes.clone(),
+            }),
+            Packet::Unsubscribe(unsubscribe) => mqttrs::Packet::Unsubscribe(mqttrs::Unsubscribe {
+                pid: unsubscribe.pid,
+                topics: unsubscribe.topics.clone(),
+            }),
+            Packet::Unsuback(pid) => mqttrs::Packet::Unsuback(*pid),
+            Packet::Pingreq => mqttrs::Packet::Pingreq,
+            Packet::Pingresp => mqttrs::Packet::Pingresp,
+            Packet::Disconnect => mqttrs::Packet::Disconnect,
+        }
+    }
+}
+
+impl From<mqttrs::Packet<'_>> for Packet {
+    fn from(pkt: mqttrs::Packet<'_>) -> Self {
+        match pkt {
+            mqttrs::Packet::Connack(connack) => Packet::Connack(connack),
+            mqttrs::Packet::Connect(mqttrs::Connect {
+                protocol,
+                keep_alive,
+                client_id,
+                clean_session,
+                last_will,
+                username,
+                password,
+            }) => Packet::Connect(Connect {
+                protocol,
+                keep_alive,
+                client_id: client_id.to_string(),
+                clean_session,
+                last_will: last_will.and_then(|lw| {
+                    Some(LastWill {
+                        topic: lw.topic.to_string(),
+                        message: lw.message.to_vec(),
+                        qos: lw.qos,
+                        retain: lw.retain,
+                    })
+                }),
+                username: username.and_then(|u| Some(u.to_string())),
+                password: password.and_then(|p| Some(p.to_vec())),
+            }),
+            mqttrs::Packet::Publish(mqttrs::Publish {
+                dup,
+                qospid,
+                retain,
+                topic_name,
+                payload,
+            }) => Packet::Publish(Publish {
+                dup,
+                qospid,
+                retain,
+                topic_name: topic_name.to_string(),
+                payload: payload.to_vec(),
+            }),
+            mqttrs::Packet::Puback(pid) => Packet::Puback(pid),
+            mqttrs::Packet::Pubrec(pid) => Packet::Pubrec(pid),
+            mqttrs::Packet::Pubrel(pid) => Packet::Pubrel(pid),
+            mqttrs::Packet::Pubcomp(pid) => Packet::Pubcomp(pid),
+            mqttrs::Packet::Subscribe(subscribe) => Packet::Subscribe(subscribe),
+            mqttrs::Packet::Suback(suback) => Packet::Suback(suback),
+            mqttrs::Packet::Unsubscribe(unsubscribe) => Packet::Unsubscribe(unsubscribe),
+            mqttrs::Packet::Unsuback(pid) => Packet::Unsuback(pid),
+            mqttrs::Packet::Pingreq => Packet::Pingreq,
+            mqttrs::Packet::Pingresp => Packet::Pingresp,
+            mqttrs::Packet::Disconnect => Packet::Disconnect,
+        }
+    }
+}
